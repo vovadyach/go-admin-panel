e117f6eec6cb694f0fa71b34cfface6fbde6f36c
vvdiachenko <diachenko.vladimir@gmail.com>
2026-03-03 23:37:53 -0800

    feat(web): add prettier

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,5 @@
 function App() {
-  return <h1 className="text-3xl font-bold">Hello, Go Web</h1>
-
+  return <h1 className="text-3xl font-bold">Hello, Go Web</h1>;
 }
 
-export default App
+export default App;
